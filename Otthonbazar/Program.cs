94437d5d720c9f6a8ef380e7605927c0057c12e8
Attila Bruder <bruderattila@gmail.com>
2021-09-16 19:39:11 +0200

    Ne kelljen mindig "Update-Database" parancsot hívogatni az adatb frissítéséhez. Ez minden app elindításkor automatikusan frissíti az adatbázist.

--- Otthonbazar/Program.cs
+++ Otthonbazar/Program.cs
@@ -5,16 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Otthonbazar.Data;
 
-namespace Otthonbazar
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace Otthonbazar {
+    public class Program {
+        public static void Main(string[] args) {
+            var webHost = CreateWebHostBuilder(args).Build();
+            using (var scope = webHost.Services.CreateScope()) {
+                scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
+            }
+            webHost.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
