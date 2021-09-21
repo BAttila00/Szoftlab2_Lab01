d657f1952de3900633bd658952eff7f1526cd141
Attila Bruder <bruderattila@gmail.com>
2021-09-21 21:20:34 +0200

    A házak típusánál a magyar név jelenjen meg ne az angol. Az enum osztályt kellett kiegészíteni DataAnnotations-ökkel.

--- Otthonbazar/Data/AdvertisementType.cs
+++ Otthonbazar/Data/AdvertisementType.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Otthonbazar.Data {
     public enum AdvertisementType {
+        [Display(Name = "Lakás")]
         Flat,
+        [Display(Name = "Ház")]
         House,
+        [Display(Name = "Nyaraló")]
         HolidayHouse,
+        [Display(Name = "Építési telek")]
         BuildingPlot
     }
 }
