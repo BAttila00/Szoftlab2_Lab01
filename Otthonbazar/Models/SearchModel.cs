fe46f2e941352403076f52aec12ca840f6e0edb0
Attila Bruder <bruderattila@gmail.com>
2021-09-22 19:19:53 +0200

    Keresés hírdetésekre.

---
+++ Otthonbazar/Models/SearchModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Otthonbazar.Models {
+    public class SearchModel {
+        [Display(Name = "Város Id")]
+        public int? CityId { get; set; }
+        [Display(Name = "Város név")]
+        public string CityName { get; set; }
+        [Display(Name = "Maximum ár")]
+        public decimal? PriceMax { get; set; }
+        [Display(Name = "Minimum ár")]
+        public decimal? PriceMin { get; set; }
+        [Display(Name = "Szobák maximum száma")]
+        public int? RoomMax { get; set; }
+        [Display(Name = "Szobák minimum száma")]
+        public int? RoomMin { get; set; }
+        [Display(Name = "Maximális méret")]
+        public int? SizeMax { get; set; }
+        [Display(Name = "Minimális méret")]
+        public int? SizeMin { get; set; }
+    }
+}
--- Otthonbazar/Pages/Advertisements/Index.cshtml.cs
+++ Otthonbazar/Pages/Advertisements/Index.cshtml.cs
@@ -6,12 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Otthonbazar.Data;
+using Otthonbazar.Models;
 
 namespace Otthonbazar.Pages.Advertisements
 {
     public class IndexModel : PageModel
     {
         private readonly Otthonbazar.Data.ApplicationDbContext _context;
+
+        [BindProperty(SupportsGet = true)]
+        public SearchModel Search { get; set; }
 
         public IndexModel(Otthonbazar.Data.ApplicationDbContext context)
         {
@@ -24,6 +28,28 @@ namespace Otthonbazar.Pages.Advertisements
         {
             Advertisement = await _context.Advertisements
                 .Include(a => a.City).ToListAsync();
+
+            IQueryable<Advertisement> advertisements = _context.Advertisements.Include(a => a.City);
+
+            if (Search.CityName != null)
+                advertisements = advertisements.Where(a => a.City.Name.Equals(Search.CityName));
+
+            if (Search.PriceMin != null)
+                advertisements = advertisements.Where(a => a.Price >= Search.PriceMin.Value);
+            if (Search.PriceMax != null)
+                advertisements = advertisements.Where(a => a.Price <= Search.PriceMax.Value);
+
+            if (Search.SizeMin != null)
+                advertisements = advertisements.Where(a => a.Size >= Search.SizeMin.Value);
+            if (Search.SizeMax != null)
+                advertisements = advertisements.Where(a => a.Size <= Search.SizeMax.Value);
+
+            if (Search.RoomMin != null)
+                advertisements = advertisements.Where(a => a.Room >= Search.RoomMin.Value);
+            if (Search.RoomMax != null)
+                advertisements = advertisements.Where(a => a.Room <= Search.RoomMax.Value);
+
+            Advertisement = await advertisements.ToListAsync();
         }
     }
 }
